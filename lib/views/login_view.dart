6b5db247a20d21f018a01a9091931544d8227d36
Amanshu Jaiswal <202051023@iiitvadodara.ac.in>
2023-04-07 02:58:40 +0530

    step-5

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
+import 'package:mynotes/constants/routes.dart';
+import '../utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -63,19 +64,27 @@ class _LoginViewState extends State<LoginView> {
                   email: email,
                   password: password,
                 );
-                Navigator.of(context).pushReplacementNamed('/notes');
-                devtools.log(usercredential.toString());
+                Navigator.of(context).pushReplacementNamed(notesRoute);
               } on FirebaseAuthException catch (e){
-                if(e.code == 'user not found')
-                  devtools.log('user not found');
-                else if(e.code == 'wrong password')
-                  devtools.log('wrong password');
+                if(e.code == 'user-not-found') {
+                  await showErrorDialog(context, 'User not found',);
+                } else if(e.code == 'wrong-password') {
+                  await showErrorDialog(context, 'Wrong Credentials',);
+                }
+                // for exceptions in firebaseAuth other than wrong user name and password
+                 else{
+                   await showErrorDialog(context, 'Error: ${e.code} ');
+                }
+              }
+              // for exceptions other than firebaseAuth
+              catch (e){
+                await showErrorDialog(context, e.toString());
               }
             },
             child: const Text('Login'),
           ),
           TextButton(onPressed: (){
-            Navigator.of(context).pushReplacementNamed('/register');
+            Navigator.of(context).pushReplacementNamed(registerRoute);
             // its given something else in video but this worked
 
             },
