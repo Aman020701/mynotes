615d4c76f633a5432e0ffe07cd162bc7adf77621
Amanshu Jaiswal <202051023@iiitvadodara.ac.in>
2023-04-14 17:01:20 +0530

    step9

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -4,7 +4,7 @@ import 'package:flutter/cupertino.dart';
 @immutable // internals are never gonna change upon initialization
 class AuthUser{
   final bool isEmailVerified;
-  const AuthUser(this.isEmailVerified);
-  factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified); // taking reference from user in firebase class
+  const AuthUser({required this.isEmailVerified});
+  factory AuthUser.fromFirebase(User user) => AuthUser(isEmailVerified : user.emailVerified); // taking reference from user in firebase class
 
 }
